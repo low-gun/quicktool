15a97428a8e737bfb613d2c873cd450b55e909db
logan <logan@loganui-MacBookAir.local>
2025-03-12 20:12:18 +0900

    db연결

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "2313",
-  database: "quicktool_database", // 방금 만든 DB명
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = pool.promise();
